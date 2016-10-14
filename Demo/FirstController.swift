88c5814c260688eafc21192554c0dfce46b53467
符现超 <fuxianchao@gmail.com>
2016-10-14 20:18:11 +0800

    first commit

---
+++ Demo/FirstController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 符现超 on 2016/10/14.
+//  Copyright © 2016年 Zero.D.Saber. All rights reserved.
+//
+
+import UIKit
+
+class FirstController: UIViewController, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.delegate = self
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if ((self.navigationController?.delegate = self) != nil) {
+            self.navigationController?.delegate = nil
+        }
+    }
+    
+    //MARK: UINavigationControllerDelegate
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationControllerOperation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        if fromVC == self {
+            return ZDScrollTransition()
+        }
+        return nil
+    }
+}
+
---
+++ ZDTransition/ZDScrollTransition.swift
@@ -0,0 +1,58 @@
+//
+//  ZDScrollTransition.swift
+//  Demo
+//
+//  Created by Zero on 2016/10/14.
+//  Copyright © 2016年 Zero.D.Saber. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+let duration: TimeInterval = 0.3
+
+class ZDScrollTransition: NSObject, UIViewControllerAnimatedTransitioning {
+    
+    //MARK: UIViewControllerAnimatedTransitioning
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return duration;
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from)
+        let toView = transitionContext.view(forKey: UITransitionContextViewKey.to)
+        let containerView = transitionContext.containerView
+        
+        
+        // http://swift.gg/2016/04/13/swift-qa-2016-04-13/
+        var snapShortView: UIView
+        if #available(iOS 10, *) {
+            snapShortView = zd_snapShortView(fromView!)
+        } else {
+            snapShortView = (fromView?.snapshotView(afterScreenUpdates: false))!
+        }
+        
+        snapShortView.frame = CGRect(x: 0, y: 0, width: containerView.bounds.width, height: containerView.bounds.height)
+        
+        containerView.addSubview(snapShortView)
+        containerView.addSubview(toView!)
+        
+        UIView.animate(withDuration: duration, delay: 0, options: UIViewAnimationOptions.curveEaseIn, animations: { 
+            toView?.transform = CGAffineTransform(translationX: 0, y: -containerView.bounds.height/2)
+            }) { (finished) in
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+        
+    }
+    
+    //MARK: Private Method
+    private func zd_snapShortView(_ view: UIView) -> UIView {
+        UIGraphicsBeginImageContext(view.frame.size)
+        let context = UIGraphicsGetCurrentContext()
+        view.layer.render(in: context!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        let imageView = UIImageView.init(image: image)
+        return imageView
+    }
+}
